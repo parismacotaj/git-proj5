4db6c910958b1d872a27410cbf0e6055082e24b0
Paris Macotaj <parismacotaj20004@hotmail.com>
2023-12-11 19:47:51 +0200

    Main updated

--- src/gr/aueb/cf/proj5/main.java
+++ src/gr/aueb/cf/proj5/main.java
@@ -2,6 +2,6 @@ package gr.aueb.cf.proj5;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello CF");
+        System.out.println("Hello coding factory!!!!");
     }
 }
